22b8f7f024028325058a04edd30607286c4774ce
qhq <qhqcz@qq.com>
2022-02-11 21:33:32 +0800

    Update build.py

--- build.py
+++ build.py
@@ -75,7 +75,7 @@ if __name__ == '__main__':
                     # fs.copy("", str(Path(Path.joinpath(fs.workdir, moduleName))), outPath)
                 else:
                     print("module file does not exist")
-            shutil.copytree(str(Path(Path.joinpath(fs.workdir, "assets"))), str(Path(Path.joinpath(fs.workdir, "switch_out"))), dirs_exist_ok=True)
+            shutil.copytree(str(Path(Path.joinpath(Path.cwd(), "assets"))), str(Path(Path.joinpath(fs.workdir, "switch_out"))), dirs_exist_ok=True)
             # fs.copy("", str(Path.joinpath(Path.cwd(), "assets", "boot.ini")), str(Path(Path.joinpath(fs.workdir, "switch_out","boot.ini"))))
             # fs.copy("", str(Path.joinpath(Path.cwd(), "assets", "exosphere.ini")), str(Path(Path.joinpath(fs.workdir, "switch_out","exosphere.ini"))))
             print("Zipping package: " + "deepsea-"+packageName+"_v"+settings["version"])
