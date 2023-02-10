f40b71190c483362bdb12f218ecd4535a3501a10
qhq <qhqcz@qq.com>
2023-02-10 21:54:35 +0800

    Update gh.py

--- src/scripts/gh.py
+++ src/scripts/gh.py
@@ -8,13 +8,6 @@ class GH():
     def __init__(self, ghToken):
         self.token = ghToken
         self.github = Github(self.token)
-        
-    def formatGMTime(self, timestamp):
-            GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
-            local_time = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
-            now = local_time.strftime("%Y%m%d%H%M%S")
-
-            return now
 
     def downloadLatestRelease(self, moduleJson, downloadPath):
         try:
@@ -22,22 +15,23 @@ class GH():
         except:
             print("无法获取: ", moduleJson["repo"])
             return
+        
+        ghLatestTag = ghRepo.get_tags()[0]
+        
+        print( ghLatestTag.name) # v1.0
+        print( ghLatestTag.last_modified) # Fri, 23 Oct 2020 04:21:50 GMT
+        #print( i.raw_data)
+        commit = ghLatestTag.commit # Commit(sha="7e700a25a6cb378d5c04d7cb3d616c14546d1c6b")
 
-        for i in ghRepo.get_tags():
-        #for i in repo.get_releases():
-            #print(dir(i))
-            print( i.name) # v1.0
-            print( i.last_modified) # Fri, 23 Oct 2020 04:21:50 GMT
-            #print( i.raw_data)
-            commit = i.commit # Commit(sha="7e700a25a6cb378d5c04d7cb3d616c14546d1c6b")
+    #    print(commit.last_modified) # Fri, 23 Oct 2020 04:21:50 GMT
+    #    print(commit.update()) # True
 
-        #    print(commit.last_modified) # Fri, 23 Oct 2020 04:21:50 GMT
-        #    print(commit.update()) # True
-
-            createTime =  commit.stats.last_modified # # Fri, 23 Oct 2020 04:21:50 GMT
-
-            a = formatGMTime(createTime)
-            print (a) # 当前tag的时间戳 20201023122146
+        timestamp =  commit.stats.last_modified # # Fri, 23 Oct 2020 04:21:50 GMT
+        
+        GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+        local_time = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
+        now = local_time.strftime("%Y%m%d%H%M%S")
+        print(now)
 
         releases = ghRepo.get_releases()
         if releases.totalCount == 0:
