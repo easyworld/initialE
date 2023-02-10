d3c72fab18e3864fd1d5a074d2342354bdd51a3d
qhq <qhqcz@qq.com>
2023-02-10 22:23:22 +0800

    Update build.py

--- build.py
+++ build.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
                         if "url" in module:
                             print("Downloading: " + module["file"])
                             downloadedFiles = dl.downloadUrl(module, dlPath)
-                        print("downloadedFiles: " + downloadedFiles)
+                        print(downloadedFiles)
                         for customStep in module["customSteps"]:
 
                             if customStep["action"] == "createDir":
