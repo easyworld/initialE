7147f5631797b60b10d49d695d261707f0767ab9
qhq <qhqcz@qq.com>
2023-02-10 13:21:51 +0800

    Update build.py

--- build.py
+++ build.py
@@ -26,6 +26,7 @@ if __name__ == '__main__':
     for packageName in settings["packages"]:
         packageObj = settings["packages"][packageName]
         if packageObj["active"] == True:
+            print("=== packageName: " + packageName + " ===")
             for moduleName in packageObj["modules"]:
                 if fs.doesFilesExist(False, "src/modules/"+moduleName+".json"):
                     module = fs.getJson(False, "src/modules/"+moduleName+".json")
