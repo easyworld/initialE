0170c482d0126e72161bece35b5a44ee1fcc81fe
qhq <qhqcz@qq.com>
2023-02-10 21:44:00 +0800

    Update gh.py

--- src/scripts/gh.py
+++ src/scripts/gh.py
@@ -8,6 +8,13 @@ class GH():
     def __init__(self, ghToken):
         self.token = ghToken
         self.github = Github(self.token)
+        
+    def formatGMTime(timestamp):
+            GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+            local_time = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
+            now = local_time.strftime("%Y%m%d%H%M%S")
+
+            return now
 
     def downloadLatestRelease(self, moduleJson, downloadPath):
         try:
