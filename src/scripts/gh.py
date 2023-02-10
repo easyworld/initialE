a85742a29ee9924795c38858075b36d573d66a44
qhq <qhqcz@qq.com>
2023-02-10 21:40:52 +0800

    Update gh.py

--- src/scripts/gh.py
+++ src/scripts/gh.py
@@ -15,7 +15,23 @@ class GH():
         except:
             print("无法获取: ", moduleJson["repo"])
             return
-        
+
+        for i in ghRepo.get_tags():
+        #for i in repo.get_releases():
+            #print(dir(i))
+            print( i.name) # v1.0
+            print( i.last_modified) # Fri, 23 Oct 2020 04:21:50 GMT
+            #print( i.raw_data)
+            commit = i.commit # Commit(sha="7e700a25a6cb378d5c04d7cb3d616c14546d1c6b")
+
+        #    print(commit.last_modified) # Fri, 23 Oct 2020 04:21:50 GMT
+        #    print(commit.update()) # True
+
+            createTime =  commit.stats.last_modified # # Fri, 23 Oct 2020 04:21:50 GMT
+
+            a = formatGMTime(createTime)
+            print (a) # 当前tag的时间戳 20201023122146
+
         releases = ghRepo.get_releases()
         if releases.totalCount == 0:
             print("无可用版本: ", moduleJson["repo"])
