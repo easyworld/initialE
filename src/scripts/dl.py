04da58907c3cdc220c6abc87b63fbe84a967597c
qhq <qhqcz@qq.com>
2023-02-11 13:13:42 +0800

    Update dl.py

--- src/scripts/dl.py
+++ src/scripts/dl.py
@@ -1,12 +1,18 @@
 from pathlib import Path
 import urllib.request
 import re
+import datetime
 
 class DL():
     def __init__(self):
         self.name = "下载链接"
 
     def downloadUrl(self, moduleJson, downloadPath):
+
+        GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+        local_time = datetime.datetime.strptime(datetime.datetime.now(), GMT_FORMAT) + datetime.timedelta(hours=8)
+        now = local_time.strftime("%Y-%m-%d %H:%M:%S")
+
         for url in moduleJson["url"]:
             try:
                 print("- downloading: " + url)
@@ -17,4 +23,9 @@ class DL():
                 return
 
             urllib.request.urlretrieve(url, downloadFilePath)
+
+            # info = {"tag":"9.9.9","last_modified":now,"url":url}
+            # print(info)
+
+    return info
 
