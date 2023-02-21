f8a828d535a0c41e59aa4e19d778820f4a824fc8
qhq <qhqcz@qq.com>
2023-02-21 14:54:08 +0800

    Update gh.py

--- src/scripts/gh.py
+++ src/scripts/gh.py
@@ -9,7 +9,13 @@ class GH():
     def __init__(self, ghToken):
         self.token = ghToken
         self.github = Github(self.token)
-
+        
+    def formatTime(timestamp):
+        GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+        local_time = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
+        now = local_time.strftime("%Y-%m-%d %H:%M:%S")
+        return now
+        
     def downloadLatestRelease(self, moduleJson, downloadPath):
         try:
             ghRepo = self.github.get_repo(moduleJson["repo"])
@@ -27,13 +33,14 @@ class GH():
         commit = ghLatestTag.commit # Commit(sha="7e700a25a6cb378d5c04d7cb3d616c14546d1c6b")
         timestamp =  commit.stats.last_modified # # Fri, 23 Oct 2020 04:21:50 GMT
        
-        GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
-        local_time = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
-        now = local_time.strftime("%Y-%m-%d %H:%M:%S")
+        # GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+        # local_time = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
+        # now = local_time.strftime("%Y-%m-%d %H:%M:%S")
+        now = formatTime(timestamp)
         # print("last modified: " + now)
         
-        info = {"tag":ghLatestTag.name,"last_modified":now,"url": ghLatestTag.zipball_url}
-        print(info)
+        # info = {"tag":ghLatestTag.name,"last_modified":now,"url": ghLatestTag.zipball_url}
+        # print(info)
         
         releases = ghRepo.get_releases()
         if releases.totalCount == 0:
@@ -48,6 +55,9 @@ class GH():
             for asset in ghLatestRelease.get_assets():
                 if re.search(pattern, asset.name):
                     matched_asset = asset
+                    url = asset.browser_download_url
+                    info = {"tag":ghLatestTag.name,"last_modified":formatTime(updated_at),"url": url}
+                    print(info)
                     break
             if matched_asset is None:
                 print("未找到文件: ", pattern)
