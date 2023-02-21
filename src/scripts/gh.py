dd0aa6120e857b2f84b4eb8a0e062aeac359fc6f
qhq <qhqcz@qq.com>
2023-02-21 15:32:20 +0800

    Update gh.py

--- src/scripts/gh.py
+++ src/scripts/gh.py
@@ -56,7 +56,7 @@ class GH():
                 if re.search(pattern, asset.name):
                     matched_asset = asset
                     url = asset.browser_download_url
-                    info = {"tag":ghLatestTag.name,"last_modified":datetime.datetime.fromisoformat(asset.updated_at),"url": url}
+                    info = {"tag":ghLatestTag.name,"last_modified":datetime.datetime.fromisoformat(str(asset.updated_at)),"url": url}
                     print(info)
                     break
             if matched_asset is None:
